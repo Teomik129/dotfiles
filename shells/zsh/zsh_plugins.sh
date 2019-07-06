f2da12bff383b00f0057f34ade5b6ce2ce67db33
Juleo Amosah <teomik129@gmail.com>
2019-07-05 23:23:42 -0400

    Switch to advanced config

---
+++ shells/zsh/zsh_plugins.sh
@@ -0,0 +1,2 @@
+source /home/teomik/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
+fpath+=( /home/teomik/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k )
