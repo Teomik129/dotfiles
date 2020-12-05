98cf9599d6a757fcc27386835216bd9bb7d63ad1
Juleo Amosah <teomik129@gmail.com>
2020-12-05 15:51:30 -0500

    Remove fish config
    I'm switching back to zsh...
    Also updated file permissions (YAML files don't need to be executable)

--- shells/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
